f19b7931b9ce23ea02fb69442b77514965155970
ycrep1993 <ycrep1993@gmail.com>
2016-08-31 17:56:30 +0200

    Initial commit

---
+++ src/nl/mydigitalproject/Main.java
@@ -0,0 +1,12 @@
+package nl.mydigitalproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    new Main().run();
+    }
+
+    private void run(){
+        System.out.println("yolo");
+    }
+}
